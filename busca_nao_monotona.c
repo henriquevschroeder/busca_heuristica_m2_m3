bd1f4f173e726c0c67b3d1ede76c496e8141c9ae
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2026-08-09 02:37:16 -0300

    Update header with student names
    Added student names to the file header.

--- busca_nao_monotona.c
+++ busca_nao_monotona.c
@@ -3,6 +3,12 @@
  *
  * Universidade do Vale do Itajaí - Complexidade de Algoritmos
  * Atividade de Busca Heurística
+ * 
+ * **Alunos:**
+ * - Henrique Schroeder
+ * - Nilson Andrade
+ * - Matheus Barbiéri
+ * - Vinícius Grisa
  *
  * BUSCA LOCAL NÃO-MONÓTONA (TÊMPERA SIMULADA / SIMULATED ANNEALING)
  * ---------------------------------------------------------------------------
